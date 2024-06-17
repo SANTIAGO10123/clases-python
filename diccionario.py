fbe96f242a3f07289626e5dbb0cee98e8bdbd8d4
SANTIAGO10123 <losanonimostube8@gmail.com>
2024-06-17 20:23:00 +0200

    Update diccionario.py

--- diccionario.py
+++ diccionario.py
@@ -1,9 +1,9 @@
 meme_dict = {
             "CRINGE": "Algo excepcionalmente raro o embarazoso",
             "LOL": "Una respuesta común a algo gracioso",
-            "XD": 
+            "XD": "El significado del XD se refiere a una risa descontrolada, concretamente a carcajadas.",
             }
-word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!)")
 if word in meme_dict.keys():
     print(meme_dict[word])
 else:
